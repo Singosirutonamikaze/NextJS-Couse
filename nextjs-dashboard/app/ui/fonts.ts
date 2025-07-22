abaf211df8dc5c13b67090745f82f931feff361c
Singosirutonamikaze <yaodieudonnesingo6@gmail.com>
2025-07-22 10:06:05 +0000

    nouvelle apprentissage de next-js

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,12 +1,14 @@
-import {Inter} from 'next/font/google';
+import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
+  display: 'swap',        // facultatif
+  preload: false,         // facultatif
 });
 
-export const lusitana =  Lusitana({
-    weight: ['400', '700'],
-    subsets: ['latin'],
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
 });
